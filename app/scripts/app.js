3d26e7eefb8eaa77e5e76fc57ce62d4c656aaa5c
alboteanud@gmail.com <alboteanud@gmail.com>
2018-02-02 22:46:18 +0200

    nice

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -96,7 +96,8 @@
       card.querySelector('.description').textContent = data.weather[0].description;
       card.querySelector('.date').textContent = new Date(data.dt * 1000)
         .toLocaleDateString(localeDate, { weekday: 'short', month: 'short', day: 'numeric' });
-      card.querySelector('.current .icon').classList.add(app.getIconClass(data.weather[0].icon));
+        var iconCode = data.weather[0].icon;
+      card.querySelector('.current .icon').classList.add(app.getIconClass(iconCode));
       card.querySelector('.current .temperature .value').textContent =
         Math.round(data.main.temp);
       var optionsDate = { hour: 'numeric', minute: 'numeric', weekday: 'narrow' };
@@ -270,64 +271,82 @@
 
   app.getIconClass = function (weatherCode) {
     // Weather codes: https://developer.yahoo.com/weather/documentation.html#codes
-    weatherCode = parseInt(weatherCode);
+    // weatherCode = parseInt(weatherCode);
     switch (weatherCode) {
-      case 25: // cold
-      case 32: // sunny
-      case 33: // fair (night)
-      case 34: // fair (day)
-      case 36: // hot
-      case 3200: // not available
+      // case 25: // cold
+      // case 32: // sunny
+      // case 33: // fair (night)
+      // case 34: // fair (day)
+      // case 36: // hot
+      // case 3200: // not available
+      case '01d':
+      case '01n':
         return 'clear-day';
-      case 0: // tornado
-      case 1: // tropical storm
-      case 2: // hurricane
-      case 6: // mixed rain and sleet
-      case 8: // freezing drizzle
-      case 9: // drizzle
-      case 10: // freezing rain
-      case 11: // showers
-      case 12: // showers
-      case 17: // hail
-      case 35: // mixed rain and hail
-      case 40: // scattered showers
+      // case 0: // tornado
+      // case 1: // tropical storm
+      // case 2: // hurricane
+      // case 6: // mixed rain and sleet
+      // case 8: // freezing drizzle
+      // case 9: // drizzle
+      // case 10: // freezing rain
+      // case 11: // showers
+      // case 12: // showers
+      // case 17: // hail
+      // case 35: // mixed rain and hail
+      // case 40: // scattered showers
+      case '09d':
+      case '09n':
+      case '10d':
+      case '10n':
         return 'rain';
-      case 3: // severe thunderstorms
-      case 4: // thunderstorms
-      case 37: // isolated thunderstorms
-      case 38: // scattered thunderstorms
-      case 39: // scattered thunderstorms (not a typo)
-      case 45: // thundershowers
-      case 47: // isolated thundershowers
+      // case 3: // severe thunderstorms
+      // case 4: // thunderstorms
+      // case 37: // isolated thunderstorms
+      // case 38: // scattered thunderstorms
+      // case 39: // scattered thunderstorms (not a typo)
+      // case 45: // thundershowers
+      // case 47: // isolated thundershowers
+      case '11d':
+      case '11n':
         return 'thunderstorms';
-      case 5: // mixed rain and snow
-      case 7: // mixed snow and sleet
-      case 13: // snow flurries
-      case 14: // light snow showers
-      case 16: // snow
-      case 18: // sleet
-      case 41: // heavy snow
-      case 42: // scattered snow showers
-      case 43: // heavy snow
-      case 46: // snow showers
+      // case 5: // mixed rain and snow
+      // case 7: // mixed snow and sleet
+      // case 13: // snow flurries
+      // case 14: // light snow showers
+      // case 16: // snow
+      // case 18: // sleet
+      // case 41: // heavy snow
+      // case 42: // scattered snow showers
+      // case 43: // heavy snow
+      // case 46: // snow showers
+      case '13d':
+      case '13n':
         return 'snow';
-      case 15: // blowing snow
-      case 19: // dust
-      case 20: // foggy
-      case 21: // haze
-      case 22: // smoky
+      // case 15: // blowing snow
+      // case 19: // dust
+      // case 20: // foggy
+      // case 21: // haze
+      // case 22: // smoky
+      case '50d':
+      case '50n':
         return 'fog';
-      case 24: // windy
-      case 23: // blustery
+      // case 24: // windy
+      // case 23: // blustery
         return 'windy';
-      case 26: // cloudy
-      case 27: // mostly cloudy (night)
-      case 28: // mostly cloudy (day)
-      case 31: // clear (night)
+      // case 26: // cloudy
+      // case 27: // mostly cloudy (night)
+      // case 28: // mostly cloudy (day)
+      // case 31: // clear (night)
+      case '03d':
+      case '03n':
+      case '04d':
+      case '04n':
         return 'cloudy';
-      case 29: // partly cloudy (night)
-      case 30: // partly cloudy (day)
-      case 44: // partly cloudy
+      // case 29: // partly cloudy (night)
+      // case 30: // partly cloudy (day)
+      // case 44: // partly cloudy
+      case '02d':
+      case '02n':
         return 'partly-cloudy-day';
     }
   };
